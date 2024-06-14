388a9ace21ed41e5f6e8a35577ad59f591947882
Victor benitez <34097330+victor2283@users.noreply.github.com>
2024-06-14 13:31:44 -0300

    Update app_tk.py

--- app_tk.py
+++ app_tk.py
@@ -15,19 +15,9 @@ mPd = sPd * 2
 lPd = mPd * 3
 nbdevup= 2
 nbdevdn=2
-perc_stopSide= 0.03
-perc_priceSide=0.01
+perc_stopSide= 0.035
+perc_priceSide=0.018
 bot = BotBinance(symbol=symbol, asset_primary=asset_primary, asset_secundary=asset_secundary, mode_Soft=mode_Soft, interval="1m", limit=300, sPd=sPd, mPd=mPd, lPd=lPd, perc_binance= perc_binance, perc_stopSide=perc_stopSide, perc_priceSide=perc_priceSide, nbdevup=nbdevup, nbdevdn=nbdevdn)
-
-ear = 0
-price_market = 0
-last_price_market = 0
-price_buy = 0
-orderId=0
-last_order_tradeId =0
-sTrade =0
-last_trend=""
-
 
 running = False  # Estado del bot
 candles=[]
@@ -46,19 +36,18 @@ frame_bottom = tk.Frame(root)
 frame_bottom.pack(pady=10)
 
 def update_root():
-    global fig, canvas, candles, price_market, last_price_market, ear, last_order_tradeId, sTrade, last_trend, symbol, nbdevup, nbdevdn, asset_primary, asset_secundary, perc_stopSide, perc_priceSide, mode_Soft, perc_binance 
+    global fig, canvas 
     if not running:
         return
     
-    sTrade, last_order_tradeId, last_trend, last_price_market, closes, upperband, lowerband, smaS, smaM, smaL, print_msg, print_alert, print_ear, print_price_market, candles, price_market = bot.update_data(last_trend, last_price_market, last_order_tradeId, sTrade)
-                                                                                                                                                                                                      
-    print(f" [{sTrade}] | {print_price_market} | {print_ear} | {print_alert}")
+    closes, upperband, lowerband, smaS, smaM, smaL, print_msg, print_alert, print_ear, print_price_market, candles, price_market, last_price_market = bot.update_data()
+    print(f" [{print_price_market} | {print_ear} | {print_alert}")
     if print_msg !="":
         print(f"{print_msg}")
     
     # Actualizar etiquetas
     color = "green" if price_market > last_price_market else "red"
-    label_0_price.config(text= f" [{sTrade}] {print_price_market} ", fg=color)
+    label_0_price.config(text= f"{print_price_market} ", fg=color)
     label_1_ear.config(text= print_ear )
     label_2_alerts.config(text=print_alert)
     label_3_msg.config(text= print_msg, fg=color)    
