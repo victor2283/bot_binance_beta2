86e646b4d07e52849cf11c90d7bc41ed825d5074
Victor benitez <34097330+victor2283@users.noreply.github.com>
2024-06-14 13:30:41 -0300

    Update app_tx.py

--- app_tx.py
+++ app_tx.py
@@ -11,21 +11,11 @@ lPd = mPd * 3
 nbdevup= 2
 nbdevdn=2
 perc_stopSide= 0.035
-perc_priceSide=0.013
+perc_priceSide=0.018
 bot = BotBinance(symbol=symbol, asset_primary=asset_primary, asset_secundary=asset_secundary, mode_Soft=mode_Soft, interval="1m", limit=300, sPd=sPd, mPd=mPd, lPd=lPd, perc_binance= perc_binance, perc_stopSide=perc_stopSide, perc_priceSide=perc_priceSide, nbdevup=nbdevup, nbdevdn=nbdevdn)
-
-ear = 0
-price_market = 0
-last_price_market = 0
-price_buy = 0
-orderId=0
-last_order_tradeId =0
-sTrade =0
-last_trend=""
-candles=[]
 while True:
-    sTrade, last_order_tradeId, last_trend, last_price_market, closes, upperband, lowerband, smaS, smaM, smaL, print_msg, print_alert, print_ear, print_price_market, candles, price_market = bot.update_data(last_trend, last_price_market, last_order_tradeId, sTrade)
-    print(f" [{sTrade}] | {print_price_market} | {print_ear} | {print_alert}")
+    closes, upperband, lowerband, smaS, smaM, smaL, print_msg, print_alert, print_ear, print_price_market, candles, price_market, last_price_market = bot.update_data()
+    print(f" {print_price_market} | {print_ear} | {print_alert}")
     if print_msg !="":
         print(f"{print_msg}")
     time.sleep(3)
